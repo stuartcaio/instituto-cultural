4ea05b914ad817adc613cec0571b17f7eeff3221
Caio Weber Stuart <caiostuart06@gmail.com>
2022-09-12 17:37:03 -0300

    melhorando a sessão de artigos

--- src/paginas/artigos.tsx
+++ src/paginas/artigos.tsx
@@ -2,13 +2,42 @@ import React, {useState} from 'react';
 import '../reset.css';
 import '../css/App.css';
 import {Corpo, Fundo, Cabecalho, CabecalhoSuperior, Menu, Item, CabecalhoInferior, TextoDoCabecalho} from '../styles';
-import {ParagráfoSobre, Principal, Principal__Secao, Principal___Titulo, Artigos as Article, Artigo, Artigo__Imagem, Artigo__Título, Artigo__Autor, Artigo__Botao, Artigos__Botao} from '../principalStyle';
+import {Articles, Article, Article__Image, Article__Div, Article___Informacao, Article__Titulo, Article___Data, Article__Paragrafo, Article__Botao, Article__Hr} from './artigosStyle';
 import {Copyright, Rodape, Rodape__Categorias, Rodape___Categoria, Rodape____Link} from '../rodapeStyle';
 import {Link} from 'react-router-dom';
+import { Artigo__Botao } from '../principalStyle';
 
 function Artigos(){
 
-    const [artigos, setArtigos] = useState();
+    const [artigos, setArtigos] = useState([
+        {
+            id: 1,
+            imagem: require('../img/senhor_dos_aneis.jpg'),
+            titulo: 'Simbolismo em O Senhor dos Anéis',
+            categoria: 'Literatura',
+            autor: 'Caio Stuart',
+            data: '10/09/2022',
+            texto: 'ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ex dignissimos ad, illum voluptate incidunt voluptatem. Doloribus quisquam, totam voluptatem autem quas error sed deleniti magnam cupiditate, mollitia temporibus doloremque. ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor labore totam earum, quisquam, nobis soluta a dignissimos et ea repellat, tempora sint aspernatur recusandae?'
+        },
+        {
+            id: 2,
+            imagem: require('../img/topgun-maverick.jpg'),
+            titulo: 'Por que Top Gun: Maverick fez tanto sucesso?',
+            categoria: 'Cinema',
+            autor: 'Alexandre Sampaio',
+            data: '11/09/2022',
+            texto: 'ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ex dignissimos ad, illum voluptate incidunt voluptatem. Doloribus quisquam, totam voluptatem autem quas error sed deleniti magnam cupiditate, mollitia temporibus doloremque. ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor labore totam earum, quisquam, nobis soluta a dignissimos et ea repellat, tempora sint aspernatur recusandae?'
+        },
+        {
+            id: 3,
+            imagem: require('../img/monarquia-brasileira.jpg'),
+            titulo: 'Em Defesa da Monarquia Brasileira',
+            categoria: 'História',
+            autor: 'Gabriel Ovidor',
+            data: '12/09/2022',
+            texto: 'ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ex dignissimos ad, illum voluptate incidunt voluptatem. Doloribus quisquam, totam voluptatem autem quas error sed deleniti magnam cupiditate, mollitia temporibus doloremque. ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor labore totam earum, quisquam, nobis soluta a dignissimos et ea repellat, tempora sint aspernatur recusandae?'
+        }
+    ]);
 
     return(
         <>
@@ -31,6 +60,24 @@ function Artigos(){
                         <TextoDoCabecalho style={{textAlign: 'center'}}>Artigos</TextoDoCabecalho>
                     </CabecalhoInferior>
                 </Cabecalho>
+                <Articles>
+                    {artigos.map((artigo) => (
+                        <Article>
+                            <Article__Image src={artigo.imagem} />
+                            <Article__Div>
+                                <Article___Informacao>{artigo.categoria}</Article___Informacao>
+                                <Article___Informacao>{artigo.autor}</Article___Informacao>
+                                <Article___Data>{artigo.data}</Article___Data>
+                            </Article__Div>
+                            <Article__Titulo>{artigo.titulo}</Article__Titulo>
+                            <Article__Paragrafo>{artigo.texto}</Article__Paragrafo>
+                            <Link to="/paginas/artigos">
+                                <Article__Botao>Leia mais</Article__Botao>
+                            </Link>
+                            <Article__Hr />
+                        </Article>
+                    ))}
+                </Articles>
             </Fundo>
         </Corpo>
         </>
--- src/paginas/artigosStyle.tsx
+++ src/paginas/artigosStyle.tsx
@@ -1,3 +1,103 @@
 import React from 'react';
 import styled from 'styled-components';
 
+export const Articles = styled.div`
+display: flex;
+flex-direction: column;
+gap: 5rem;
+
+padding: 5rem;
+
+background-color: rgb(228, 228, 228);
+`;
+
+export const Article = styled.div`
+display: flex;
+flex-direction: column;
+gap: 1.25rem;
+`;
+
+export const Article__Image = styled.img`
+width: 40rem;
+height: 20rem;
+`;
+
+export const Article__Div = styled.div`
+display: flex;
+justify-content: start;
+align-items: center;
+gap: 0.25rem;
+
+width: 40rem;
+`;
+
+export const Article___Informacao = styled.h4`
+font-size: 1rem;
+text-align: center;
+color: grey;
+
+padding: 0.25rem;
+
+border-radius: 5px;
+border: 1px solid grey;
+
+box-sizing: border-box;
+
+width: 9rem;
+`;
+
+export const Article___Data = styled.h4`
+font-size: 1rem;
+color: grey;
+
+margin-left: 17rem;
+
+padding: 0.25rem;
+`;
+
+export const Article__Titulo = styled.h2`
+font-size: 2rem;
+font-family: 'Raleway', sans-serif;
+`;
+
+export const Article__Paragrafo = styled.p`
+font-size: 1.25rem;
+max-width: 200ch;
+overflow: hidden;
+text-overflow: ellipsis;
+white-space: nowrap;
+
+width: 55%;
+`;
+
+export const Article__Botao = styled.button`
+font-size: 1.1rem;
+text-decoration: none;
+
+padding: 0.5rem;
+
+color: grey;
+
+width: 15%;
+
+border-radius: 5px;
+border-style: 0.5px;
+border-color: grey;
+
+transition: 1s;
+
+&:hover {
+    background-color: black;
+    color: white;
+}
+`;
+
+export const Article__Hr = styled.hr`
+padding: 0rem;
+
+background-color: grey;
+
+margin-right: 40rem;
+
+width: 40rem;
+`;
